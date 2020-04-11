6edc159594f5141897efe1d2abe2ecc3903eb681
J.C. Hiatt <jc@jchiatt.com>
2020-04-11 16:16:54 -0500

    feat: add placeholders for zoom links

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -148,109 +148,109 @@ export default function Schedule({ day }) {
             <td><time>9:00am</time></td>
             <td>Stream Starts</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>9:15am</time></td>
             <td>Welcome & Announcements</td>
             <td>Emcee</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>9:30am</time></td>
             <td>Keynote: I'm Techie and I Know It</td>
             <td>Nashlie Sephus</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>10:00am</time></td>
             <td>ES8, ES9, ES10+… Let’s explore JavaScript’s exciting new features</td>
             <td>Tyler Clark</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>10:30am</time></td>
             <td>Static generated sites === great performance. What are you waiting for?</td>
             <td>Debbie O'Brien</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>11:00am</time></td>
             <td>Break</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>11:15am</time></td>
             <td>Building Your Own Design System with CSS Variables</td>
             <td>Scott Tolinski</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>11:45am</time></td>
             <td>Modern JavaScript for Modern Browsers</td>
             <td>Sia Karamalegos</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>12:15pm</time></td>
             <td>Overview: Mississippi Coding Academies</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>12:25pm</time></td>
             <td>Break, Start Q&A's</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>1:00pm</time></td>
             <td>Lunch (with Virtual Lunch Tables)</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>2:30pm</time></td>
             <td>Component Reusability at scale with StorybookJS</td>
             <td>Michael Liendo</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>3:00pm</time></td>
             <td>Progressive Enhancement in React</td>
             <td>Kayla Sween</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>3:30pm</time></td>
             <td>Adventures of Machine Learning in JavaScript</td>
             <td>Gant Laborde</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>4:00pm</time></td>
             <td>Break</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>4:15pm</time></td>
             <td>Twitter Driven Development, Redux</td>
             <td>Justin Samuels</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>4:45pm</time></td>
             <td>I Can't See: Low Vision A11y & Users</td>
             <td>Chris DeMars</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>5:15pm</time></td>
             <td>Day 1 - Closing Remarks, Start Q&A's</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
         </tbody>
       </table>
@@ -272,109 +272,109 @@ export default function Schedule({ day }) {
             <td><time>9:00am</time></td>
             <td>Stream Starts</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>9:15am</time></td>
             <td>Welcome & Announcements</td>
             <td>Emcee</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>9:30am</time></td>
             <td>Which Database? How to Choose the Best Place for your JSON Documents</td>
             <td>Suphatra Rufo</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>10:00am</time></td>
             <td>Testing Complex RxJS Async Code Using Marble Diagrams</td>
             <td>Jay Phelps</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>10:30am</time></td>
             <td>Svelte and the Great Space Elevator</td>
             <td>Shawn Wang</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>11:00am</time></td>
             <td>Break</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>11:15am</time></td>
             <td>Create Dynamic Web Apps With Serverless and the Jamstack</td>
             <td>Jason Lengstorf</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>11:45am</time></td>
             <td>Building a Full Stack CMS for the Modern Age</td>
             <td>Nader Dabit</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>12:15pm</time></td>
             <td>Break, Start Q&A's</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>1:00pm</time></td>
             <td>Lunch (with Virtual Lunch Tables)</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>2:00pm</time></td>
             <td>Handling Data with React Query</td>
             <td>Domitrius Clark</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>2:30pm</time></td>
             <td>Canvas: The Final Escape Hatch</td>
             <td>Ken Wheeler</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>3:00pm</time></td>
             <td>How to Supercharge Teams with Carefully Crafted Conventions</td>
             <td>Chris Ball</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>3:30pm</time></td>
             <td>Break</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>3:45pm</time></td>
             <td>Chasing Whales of the Command Line: The What, How, and Why of Containers 🐳</td>
             <td>Brian Holt</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>4:15pm</time></td>
             <td>Thar Be Dragons: Rebuilding Native UIs on the Web</td>
             <td>Chance Strickland</td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>4:45pm</time></td>
             <td>Zoom Chatbots</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
           <tr>
             <td><time>5:15pm</time></td>
             <td>Day 2 — Closing Remarks, Giveaways</td>
             <td></td>
-            <td></td>
+            <td>Coming soon</td>
           </tr>
         </tbody>
       </table>
