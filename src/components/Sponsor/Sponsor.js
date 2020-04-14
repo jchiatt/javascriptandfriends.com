3c7516b4e29c444b0163573cb741f9f314ee5bfc
J.C. Hiatt <jc@jchiatt.com>
2020-04-14 09:46:50 -0500

    feat: add zoom

--- src/components/Sponsor/Sponsor.js
+++ src/components/Sponsor/Sponsor.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Box } from '@rebass/grid';
 
-function Sponsor({image, link}) {
+function Sponsor({image, link, styles}) {
   return (
-    <Box px={4} style={{maxWidth: '320px'}}>
+    <Box px={4} style={{maxWidth: '320px', ...styles}}>
       <a href={link} target='_blank'>
         <img src={image} />
       </a>
