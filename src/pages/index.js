335feca304b7e276d9302325903723803f4f038f
J.C. Hiatt <jc@jchiatt.com>
2020-04-14 23:23:13 -0500

    add q&a schedules

--- src/pages/index.js
+++ src/pages/index.js
@@ -71,35 +71,35 @@ const Root = () => (
         <Text pt={10} pb={30}>All talks will happen on our main Zoom Webinar link. Other events, such as Virtual Sponsor Booths, Q&A's, Virtual Lunch Tables, etc. will have their own respective Zoom meeting links.</Text>
         <Text pt={10} pb={30}>We will have a Watercooler running at all times for anyone who wants to pop in and get to know others. Please adhere to our Code of Conduct.</Text>
         <Heading color="mainBackground" size={2}>
-          Schedule - April 16 (Day 1)
+          Schedule - April 16 (Day 1) - <a href="https://us02web.zoom.us/j/82392215017?pwd=cXpFYkhHSVR5aEptWVpidGRpUlY3dz09" target="_blank" rel="noopener noreferrer">Click here to open Day 1 Watercooler</a>
         </Heading>
         <Heading color="mainBackground" size={3}>
           Virtual Sponsor Booths
         </Heading>
         <Schedule day="sponsors-one" />
         <Heading color="mainBackground" size={3}>
-          Talks
+          Talks - <a href="https://us02web.zoom.us/j/86916890389?pwd=L1YrS0xwTGh2ZmphZkxYbjUxTGpodz09" target="_blank" rel="noopener noreferrer">Click here to open Day 1 Zoom webinar</a>
         </Heading>
         <Schedule day="one" />
-        {/* <Heading color="mainBackground" size={3}>
+        <Heading color="mainBackground" size={3}>
           Speaker Q&A
         </Heading>
-        <Schedule day="qa-one" /> */}
+        <Schedule day="qa-one" />
         <Heading color="alternateHeading" size={2}>
-          Schedule - April 17 (Day 2)
+          Schedule - April 17 (Day 2) - <a href="https://us02web.zoom.us/j/82441209987?pwd=YldYeG9CaDh2aU9wNmg5MTlaeGREUT09" target="_blank" rel="noopener noreferrer">Click here to open Day 2 Watercooler</a>
         </Heading>
         <Heading color="alternateHeading" size={3}>
           Virtual Sponsor Booths
         </Heading>
         <Schedule day="sponsors-two" />
         <Heading color="alternateHeading" size={3}>
-          Talks
+          Talks - <a href="https://us02web.zoom.us/j/82810507172?pwd=NGZNYUJwOHlqd2xWWGRVRnl6Sk9Odz09" target="_blank" rel="noopener noreferrer">Click here to open Day 2 Zoom webinar</a>
         </Heading>
         <Schedule day="two" />
-        {/* <Heading color="mainBackground" size={3}>
+        <Heading color="alternateHeading" size={3}>
           Speaker Q&A
         </Heading>
-        <Schedule day="qa-two" /> */}
+        <Schedule day="qa-two" />
       </Alternate>
       <Alternate pb={100}>
         <Heading color="alternateHeading" size={2}>
